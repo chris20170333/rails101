414a6dc7db81333fd339425e1053c44b794aca07
ouyangronglan <chris@ouyangronglandeMacBook-Pro.local>
2017-04-08 12:11:27 +0800

    implement groups#show

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,7 +1,26 @@
-require 'test_helper'
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
 
-class GroupsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+ def show
+   @group = Group.find(params[:id])
+ end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
